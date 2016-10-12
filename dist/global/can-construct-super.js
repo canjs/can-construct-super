08d705a09721343d1cee0d01bfabceacb90e1a4b
John Gardner <john@ardea.io>
2016-10-12 14:49:49 -0500

    3.0.0-pre.2

---
+++ dist/global/can-construct-super.js
@@ -0,0 +1,155 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val){
+		var parts = name.split("."),
+			cur = global,
+			i, part, next;
+		for(i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if(!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod){
+		if(!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, "default": true };
+		for(var p in mod) {
+			if(!esProps[p]) return false;
+		}
+		return true;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if(globalExport && !get(globalExport)) {
+			if(useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-construct-super@3.0.0-pre.1#can-construct-super*/
+define('can-construct-super', function (require, exports, module) {
+    (function (global) {
+        var can = require('can-util');
+        var Construct = require('can-construct');
+        var isFunction = can.isFunction, fnTest = /xyz/.test(function () {
+                return this.xyz;
+            }) ? /\b_super\b/ : /.*/, getset = [
+                'get',
+                'set'
+            ], getSuper = function (base, name, fn) {
+                return function () {
+                    var tmp = this._super, ret;
+                    this._super = base[name];
+                    ret = fn.apply(this, arguments);
+                    this._super = tmp;
+                    return ret;
+                };
+            };
+        Construct._defineProperty = function (addTo, base, name, descriptor) {
+            var _super = Object.getOwnPropertyDescriptor(base, name);
+            if (_super) {
+                can.each(getset, function (method) {
+                    if (isFunction(_super[method]) && isFunction(descriptor[method])) {
+                        descriptor[method] = getSuper(_super, method, descriptor[method]);
+                    } else if (!isFunction(descriptor[method])) {
+                        descriptor[method] = _super[method];
+                    }
+                });
+            }
+            Object.defineProperty(addTo, name, descriptor);
+        };
+        Construct._overwrite = function (addTo, base, name, val) {
+            addTo[name] = isFunction(val) && isFunction(base[name]) && fnTest.test(val) ? getSuper(base, name, val) : val;
+        };
+        module.exports = Construct;
+    }(function () {
+        return this;
+    }()));
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
